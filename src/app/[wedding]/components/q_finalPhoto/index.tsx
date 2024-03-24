9f5f9ab5d35011d2f4b6d5fff9b02bb37eb08674
yooh <yoonhwan.oh@cmesroboticsd.ai>
2024-03-25 05:20:17 +0900

    implement final photo to image

--- src/app/[wedding]/components/q_finalPhoto/index.tsx
+++ src/app/[wedding]/components/q_finalPhoto/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import getImageUrl from "@/app/lib/getImageUrl";
 import styles from "./finalPhoto.module.css";
 import Image from "next/image";
 import { useRef } from "react";
@@ -16,11 +15,13 @@ export default function FinalPhoto({
   finalPhotoColor: string;
 }) {
   const conRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
   useScrollFadeIn(conRef);
 
   return (
     <div className={styles.container + " con"} ref={conRef}>
       <Image
+        ref={imageRef}
         sizes={"100vw"}
         src={process.env.NEXT_PUBLIC_IMAGE_URL + src.replaceAll("\\", "/")}
         alt="finalPhoto"
